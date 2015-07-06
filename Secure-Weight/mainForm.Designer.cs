e16396e0f3eb8c1b014b99c0b50e264dd1466990
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2015-07-05 22:45:07 -0400

    Initial Project
    -Created the initial Visual Studio project
    -Setup WiimoteLib to be used and connect to/display data from the Wii
    Balance Board
    -Setup the main screen to show basic feedback

---
+++ Secure-Weight/mainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Secure_Weight
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connStatLbl = new System.Windows.Forms.Label();
+            this.weightLbl = new System.Windows.Forms.Label();
+            this.weightValLbl = new System.Windows.Forms.Label();
+            this.personLbl = new System.Windows.Forms.Label();
+            this.personValLbl = new System.Windows.Forms.Label();
+            this.weightImage = new System.Windows.Forms.PictureBox();
+            this.topLeftVal = new System.Windows.Forms.Label();
+            this.topRightVal = new System.Windows.Forms.Label();
+            this.bottomLeftVal = new System.Windows.Forms.Label();
+            this.bottomRightVal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.weightImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connStatLbl
+            // 
+            this.connStatLbl.AutoSize = true;
+            this.connStatLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connStatLbl.Location = new System.Drawing.Point(12, 9);
+            this.connStatLbl.Name = "connStatLbl";
+            this.connStatLbl.Size = new System.Drawing.Size(157, 26);
+            this.connStatLbl.TabIndex = 0;
+            this.connStatLbl.Text = "Not Connected";
+            // 
+            // weightLbl
+            // 
+            this.weightLbl.AutoSize = true;
+            this.weightLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLbl.Location = new System.Drawing.Point(12, 35);
+            this.weightLbl.Name = "weightLbl";
+            this.weightLbl.Size = new System.Drawing.Size(86, 26);
+            this.weightLbl.TabIndex = 1;
+            this.weightLbl.Text = "Weight:";
+            // 
+            // weightValLbl
+            // 
+            this.weightValLbl.AutoSize = true;
+            this.weightValLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightValLbl.Location = new System.Drawing.Point(104, 35);
+            this.weightValLbl.Name = "weightValLbl";
+            this.weightValLbl.Size = new System.Drawing.Size(53, 26);
+            this.weightValLbl.TabIndex = 2;
+            this.weightValLbl.Text = "0 kg";
+            // 
+            // personLbl
+            // 
+            this.personLbl.AutoSize = true;
+            this.personLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personLbl.Location = new System.Drawing.Point(12, 61);
+            this.personLbl.Name = "personLbl";
+            this.personLbl.Size = new System.Drawing.Size(87, 26);
+            this.personLbl.TabIndex = 3;
+            this.personLbl.Text = "Person:";
+            // 
+            // personValLbl
+            // 
+            this.personValLbl.AutoSize = true;
+            this.personValLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personValLbl.Location = new System.Drawing.Point(105, 61);
+            this.personValLbl.Name = "personValLbl";
+            this.personValLbl.Size = new System.Drawing.Size(64, 26);
+            this.personValLbl.TabIndex = 4;
+            this.personValLbl.Text = "None";
+            // 
+            // weightImage
+            // 
+            this.weightImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.weightImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.weightImage.Location = new System.Drawing.Point(17, 90);
+            this.weightImage.Name = "weightImage";
+            this.weightImage.Size = new System.Drawing.Size(595, 339);
+            this.weightImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.weightImage.TabIndex = 5;
+            this.weightImage.TabStop = false;
+            // 
+            // topLeftVal
+            // 
+            this.topLeftVal.AutoSize = true;
+            this.topLeftVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLeftVal.Location = new System.Drawing.Point(466, 9);
+            this.topLeftVal.Name = "topLeftVal";
+            this.topLeftVal.Size = new System.Drawing.Size(71, 26);
+            this.topLeftVal.TabIndex = 6;
+            this.topLeftVal.Text = "0.0 kg";
+            // 
+            // topRightVal
+            // 
+            this.topRightVal.AutoSize = true;
+            this.topRightVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topRightVal.Location = new System.Drawing.Point(542, 9);
+            this.topRightVal.Name = "topRightVal";
+            this.topRightVal.Size = new System.Drawing.Size(71, 26);
+            this.topRightVal.TabIndex = 7;
+            this.topRightVal.Text = "0.0 kg";
+            // 
+            // bottomLeftVal
+            // 
+            this.bottomLeftVal.AutoSize = true;
+            this.bottomLeftVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLeftVal.Location = new System.Drawing.Point(466, 61);
+            this.bottomLeftVal.Name = "bottomLeftVal";
+            this.bottomLeftVal.Size = new System.Drawing.Size(71, 26);
+            this.bottomLeftVal.TabIndex = 8;
+            this.bottomLeftVal.Text = "0.0 kg";
+            // 
+            // bottomRightVal
+            // 
+            this.bottomRightVal.AutoSize = true;
+            this.bottomRightVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomRightVal.Location = new System.Drawing.Point(542, 61);
+            this.bottomRightVal.Name = "bottomRightVal";
+            this.bottomRightVal.Size = new System.Drawing.Size(71, 26);
+            this.bottomRightVal.TabIndex = 9;
+            this.bottomRightVal.Text = "0.0 kg";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.bottomRightVal);
+            this.Controls.Add(this.bottomLeftVal);
+            this.Controls.Add(this.topRightVal);
+            this.Controls.Add(this.topLeftVal);
+            this.Controls.Add(this.weightImage);
+            this.Controls.Add(this.personValLbl);
+            this.Controls.Add(this.personLbl);
+            this.Controls.Add(this.weightValLbl);
+            this.Controls.Add(this.weightLbl);
+            this.Controls.Add(this.connStatLbl);
+            this.Name = "mainForm";
+            this.Text = "Secure Weight";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.weightImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label connStatLbl;
+        private System.Windows.Forms.Label weightLbl;
+        private System.Windows.Forms.Label weightValLbl;
+        private System.Windows.Forms.Label personLbl;
+        private System.Windows.Forms.Label personValLbl;
+        private System.Windows.Forms.PictureBox weightImage;
+        private System.Windows.Forms.Label topLeftVal;
+        private System.Windows.Forms.Label topRightVal;
+        private System.Windows.Forms.Label bottomLeftVal;
+        private System.Windows.Forms.Label bottomRightVal;
+    }
+}
+
---
+++ Secure-Weight/mainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiimoteLib;
+
+namespace Secure_Weight
+{
+    public partial class mainForm : Form
+    {
+        private Wiimote balanceBoard;
+        private Timer updateTimer;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            this.balanceBoard = new Wiimote();
+            this.updateTimer = new Timer();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.balanceBoard.Connect();
+                this.balanceBoard.SetLEDs(1);
+                this.connStatLbl.Text = "Connected";
+
+                this.updateTimer.Tick += updateTimer_Tick;
+                this.updateTimer.Start();
+            }
+            catch
+            {
+                this.connStatLbl.Text = "Not Connected";
+            }
+        }
+
+        void updateTimer_Tick(object sender, EventArgs e)
+        {
+            BalanceBoardState state = this.balanceBoard.WiimoteState.BalanceBoardState;
+            BalanceBoardSensorsF values = state.SensorValuesKg;
+
+            this.weightValLbl.Text = Math.Round(state.WeightKg, 1) + " kg";
+            this.topLeftVal.Text = Math.Round(values.TopLeft, 1) + " kg";
+            this.topRightVal.Text = Math.Round(values.TopRight, 1) + " kg";
+            this.bottomLeftVal.Text = Math.Round(values.BottomLeft, 1) + " kg";
+            this.bottomRightVal.Text = Math.Round(values.BottomRight, 1) + " kg";
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.balanceBoard.SetLEDs(0);
+        }
+    }
+}
